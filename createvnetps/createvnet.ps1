06a316c913d3f9a28c6632090311265499e26bc1
Nikola Stjelja <admin@6GPYZW1.europasystems.local>
2016-11-10 10:52:21 +0100

    Ps script to create VNet and a template to create a VM in that new VNET

---
+++ createvnetps/createvnet.ps1
@@ -0,0 +1,18 @@
+﻿Param (
+    [string] $subscriptionId,
+    [string] $resourceGroupName
+
+)
+Login-AzureRmAccount -SubscriptionId  $subscriptionId
+New-AzureRmResourceGroup -Name $resourceGroupName -Location 'West Europe'
+
+$vnet = New-AzureRmVirtualNetwork -Name HQTestVNet -ResourceGroupName $resourceGroupName -Location 'West Europe' -AddressPrefix 10.1.0.0/16
+$vnet | Out-String
+
+$subNet = New-AzureRmVirtualNetworkSubnetConfig -Name FrontEnd -AddressPrefix 10.1.0.0/24 
+$subNet | Out-String
+
+Add-AzureRmVirtualNetworkSubnetConfig -Name $subNet.Name -AddressPrefix $subNet.AddressPrefix -VirtualNetwork $vnet
+
+Set-AzureRMVirtualNetwork -VirtualNetwork $vnet
+
